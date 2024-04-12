b21992559d972b8d3d41e10446477ca347c3c26b
hrishd <hrishd@abv.bg>
2024-04-12 18:54:17 +0300

    get the context of the canvas

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 //Select the canvas
 const canvas = document.querySelector("canvas");
 
+//The canvus context
+const canvContext = canvas.getContext('2d');
+
 //Set its width and height to the full width and height of the page
 canvas.width = innerWidth;
 canvas.height = innerHeight;
