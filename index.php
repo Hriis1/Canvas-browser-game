83853802560fee8d33cf5db32b549942b3c5cb6b
hrishd <hrishd@abv.bg>
2024-04-12 19:01:58 +0300

    created a basic player class with constructor

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 
 <body>
     <canvas></canvas>
+    <script src="js/player.js"></script>
     <script src="js/main.js"></script>
 </body>
 
--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 //Select the canvas
 const canvas = document.querySelector("canvas");
 
-//The canvus context
-const canvContext = canvas.getContext('2d');
-
 //Set its width and height to the full width and height of the page
 canvas.width = innerWidth;
 canvas.height = innerHeight;
+
+//The canvus context
+const canvContext = canvas.getContext('2d');
+
+//Instantiate the player
+const player = new Player(100,100, 30, 'blue');
+
