a44842abcc48cfc81a9c8ed58e9ed372195ad30e
hrishd <hrishd@abv.bg>
2024-04-17 11:47:33 +0300

    added a game over menu

--- index.php
+++ index.php
@@ -11,6 +11,13 @@
 
 <body>
     <div class="fixed text-white text-2xl ml-3 mt-3"><span>Score: </span><span id="scoreText">0</span></div>
+    <div class="gameOverMenu" >
+        <div class="bg-white max-w-md w-full p-6 text-center">
+            <h1 id="gameOverScoreText" class="text-4xl font-bold">0</h1>
+            <p class="text-gray-700">Points</p>
+            <button id="startGameBtn" class="bg-blue-500 text-white w-full mt-4 py-4 rounded-full">Start game</button>
+        </div>
+    </div>
     <canvas></canvas>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"
         integrity="sha512-7eHRwcbYkK4d9g/6tD/mhkf++eoTHwpNM9woBxtPUBWm67zeAfFC+HrdoE2GanKeocly/VxeLvIqwvCdk7qScg=="
